3a9a4ba1b518c819a483d857f61a322f063f1076
Akashgill3 <akg650@mail.usask.ca>
2025-01-24 23:45:35 -0500

    feat: made basic ast and parser

---
+++ parser/parser.go
@@ -0,0 +1,54 @@
+package parser
+
+import (
+	"github.com/Akashgill3/monkey-interpreter/ast"
+	"github.com/Akashgill3/monkey-interpreter/lexer"
+	"github.com/Akashgill3/monkey-interpreter/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() ast.Statement {
+	return nil
+}
